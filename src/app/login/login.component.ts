d1a15ade735b2ce02d436c4b5c05027b0838f0d6
Hazel <hazelina.jelen@canang.com.my>
2018-11-02 11:11:10 +0800

    (1) updated login.component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from '../../services/authentication.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -9,59 +11,78 @@ import {AuthenticationService} from '../../services/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
-  username: string;
-  password: string;
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
   redirectUrl: string;
+  error = '';
 
-  // constructor(private router: Router,
-  //             private route: ActivatedRoute,
-  //             private authenticationService: AuthenticationService,
-  //   private authorizationService: AuthorizationService,
-  //   private identityService: IdentityService) {
-  //   this.redirectUrl = this.route.snapshot.queryParams['redirectTo'];
-  // }
-
-  constructor(private router: Router,
-              private route: ActivatedRoute) {
-    this.redirectUrl = this.route.snapshot.queryParams['redirectTo'];
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private route: ActivatedRoute,
+              private authenticationService: AuthenticationService) {
   }
 
   ngOnInit(): void {
     console.log('login page ngOnInit');
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
     // this.identityService.logout();
+
+    // reset login status
+    this.authenticationService.logout();
+
+    // get return url from route parameters or default to '/'
+    this.redirectUrl = this.route.snapshot.queryParams['redirectTo'] || '/';
   }
 
-  register(): void {
-    this.router.navigate(['registration']);
-  }
-
-  login(): void {
-    this.router.navigate(['/dashboard']);
-  }
-
-  // login(): void {
-  //   this.authenticationService.login(this.username, this.password)
-  //     .map(
-  //       (result) => {
-  //         if (result) {
-  //           this.identityService.login(result);
-  //           this.navigateAfterSuccess();
-  //         } else {
-  //           console.log('no error');
-  //         }
-  //       },
-  //       (error) => {
-  //         console.log('error:' + error);
-  //       },
-  //     ).toPromise();
+  // register(): void {
+  //   this.router.navigate(['registration']);
   // }
 
-  navigateAfterSuccess(): void {
-    if (this.redirectUrl) {
-      this.router.navigateByUrl(this.redirectUrl);
-    } else {
-      this.router.navigate(['/']);
+  get f() {return this.loginForm.controls;}
+
+  login(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
     }
+
+    this.loading = true;
+    this.authenticationService.login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.redirectUrl]);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        });
+    //   .map(
+    //     (result) => {
+    //       if (result) {
+    //         this.identityService.login(result);
+    //         this.navigateAfterSuccess();
+    //       } else {
+    //         console.log('no error');
+    //       }
+    //     },
+    //     (error) => {
+    //       console.log('error:' + error);
+    //     },
+    //   ).toPromise();
   }
+
+  // navigateAfterSuccess(): void {
+  //   if (this.redirectUrl) {
+  //     this.router.navigateByUrl(this.redirectUrl);
+  //   } else {
+  //     this.router.navigate(['/']);
+  //   }
+  // }
 
 }
